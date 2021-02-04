159e6dfe15934a3023c9b4d0d3897b722901527e
Karlis Lukstins <karlis@lukstins.com>
2021-02-04 08:58:33 +0200

    Initial Commit

---
+++ FindMyFriends/FindMyFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyFriendsApp.swift
+//  FindMyFriends
+//
+//  Created by Karlis Lukstins on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct FindMyFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
